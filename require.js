23a4400143757cacaac34047ef34c206c3dfc9ef
vin100bk <vin100.beck@gmail.com>
2015-02-11 18:40:51 +0100

    Create require.js

---
+++ require.js
@@ -0,0 +1,41 @@
+/**
+ * Allows to require easily JS modules by their name
+ * @type {{modules: {}, modulesLoaded: {}, init: Function, get: Function}}
+ */
+var require = {
+    /**
+     *  Contains modules
+     */
+    modules: {},
+
+    /**
+     * Contains information if a module is loaded or not
+     */
+    modulesLoaded: {},
+
+    /**
+     * Init modules
+     * @param config
+     */
+    init: function (config) {
+        this.modules = config;
+    },
+
+    /**
+     * Get a module
+     * @param name
+     */
+    get: function (name) {
+        if (!(name in this.modulesLoaded)) {
+            $.ajax({
+                url: global.baseUrlAssets + '/assets/js/' + this.modules[name],
+                dataType: 'script',
+                async:false
+            })
+                .fail(function (jqxhr, settings, exception) {
+                    throw 'Error while loading ' + name + ' module !';
+                });
+            this.modulesLoaded[name] = 1;
+        }
+    }
+};
